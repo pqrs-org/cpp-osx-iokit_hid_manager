12ca5512a79508b77f6709025b08173a896d375c
Takayama Fumihiko <tekezo@pqrs.org>
2019-06-02 22:29:27 +0900

    version 2.7.0

--- include/pqrs/osx/iokit_hid_manager.hpp
+++ include/pqrs/osx/iokit_hid_manager.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::iokit_hid_manager v2.6
+// pqrs::iokit_hid_manager v2.7
 
 // (C) Copyright Takayama Fumihiko 2018.
 // Distributed under the Boost Software License, Version 1.0.
