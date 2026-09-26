234397fc750e0bad532af28e1fd85c7af820b121
Takayama Fumihiko <tekezo@pqrs.org>
2026-09-26 19:52:40 +0900

    Use dispatcher_client_constructor_exception_guard

--- include/pqrs/osx/iokit_hid_manager.hpp
+++ include/pqrs/osx/iokit_hid_manager.hpp
@@ -19,6 +19,10 @@
 
 namespace pqrs::osx {
 class iokit_hid_manager final : public dispatcher::extra::dispatcher_client {
+private:
+  // Keep the guard first so member initialization failures also detach.
+  pqrs::dispatcher::extra::dispatcher_client_constructor_exception_guard dispatcher_client_constructor_exception_guard_{*this};
+
 public:
   // Signals (invoked from the dispatcher thread)
 
@@ -38,6 +42,7 @@ public:
         run_loop_thread_(run_loop_thread),
         matching_dictionaries_(matching_dictionaries),
         device_matched_delay_(device_matched_delay) {
+    dispatcher_client_constructor_exception_guard_.initialize();
   }
 
   ~iokit_hid_manager() override {
